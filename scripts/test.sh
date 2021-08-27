5809ac6c3d642f097a5107b6421e6fe521b6ce31
Yury Toporov <gemuniongroup@gmail.com>
2021-08-27 13:23:00 +0800

    refactor: add postgres and mongo CI test services

---
+++ scripts/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+echo -e "\033[34mTesting...\n\033[0m";
+
+set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
+
+export NODE_ENV=$NODE_ENV
+export POSTGRES_URL=$POSTGRES_URL
+export MONGO_URL=$MONGO_URL
+
+
+lerna bootstrap --hoist
+
+lerna run lint --stream --parallel
+
+lerna exec -- npm run test
