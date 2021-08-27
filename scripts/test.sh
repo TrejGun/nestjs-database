2d46d57c75b2bcc0756d205ce9937dca5a8fc852
Yury Toporov <gemuniongroup@gmail.com>
2021-08-27 17:55:16 +0800

    fix: jest timeout 10k

--- scripts/test.sh
+++ scripts/test.sh
@@ -14,4 +14,4 @@ lerna bootstrap --hoist
 
 # lerna run lint --stream --parallel
 
-lerna exec --stream -- npm run test -- --forceExit --runInBand
+lerna exec --stream -- npm run test -- --forceExit --runInBand --testTimeout=10000
